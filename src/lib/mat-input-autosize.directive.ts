6ca9cfb812dde6408093d8b198519b5118f011c5
Ellie Larson <ellie@polklarson.com>
2020-09-07 17:37:14 -0500

    update mat-input-autosize.directive

--- src/lib/mat-input-autosize.directive.ts
+++ src/lib/mat-input-autosize.directive.ts
@@ -1,75 +1,40 @@
-import {AfterContentInit, AfterViewInit, ContentChild, Directive, ElementRef, Input, OnDestroy} from '@angular/core';
+import {
+	AfterViewInit,
+	Directive,
+	ElementRef,
+	Input,
+	OnDestroy,
+	Optional,
+	Self
+} from '@angular/core';
 import {NgModel} from '@angular/forms';
 import {MatInput} from '@angular/material/input';
-
 import {Subscription} from 'rxjs';
 
 @Directive({
     selector: '[matInputAutosize]'
 })
-export class MatInputAutosizeDirective implements AfterViewInit, AfterContentInit, OnDestroy {
-    element: ElementRef;
+export class MatInputAutosizeDirective implements AfterViewInit, OnDestroy {
+    inputElement: HTMLInputElement;
     hiddenText: Text;
     changeDetectSub: Subscription;
 
     @Input() matInputAutosizeMaxWidth;
     @Input() matInputAutosizeMinWidth = '120px';
-    @ContentChild(NgModel) childNgModel: NgModel;
-    @ContentChild(MatInput) childMatInput: MatInput;
+    @Input() matInputAutosizeFontStyle;
 
-    constructor(el: ElementRef) {
-        this.element = el;
+    constructor(el: ElementRef<HTMLInputElement>,
+				@Self() private childMatInput: MatInput,
+				@Optional() @Self() private childNgModel: NgModel) {
+		this.inputElement = el.nativeElement;
     }
 
     ngAfterViewInit() {
-        let el = this.element.nativeElement;
-
-        if (!(el instanceof HTMLInputElement)) {
-            throw new Error('matInputAutosize directive must be on an input element');
-        }
-
-        while (el.nodeName.toUpperCase() !== 'MAT-FORM-FIELD') {
-            if (!el.parentNode) {
-                throw new Error('Element with matInputAutosize directive must be within a mat-form-field');
-            }
-
-            el = el.parentNode;
-        }
-
-        const relativeContainer = document.createElement('div');
-        Object.assign(relativeContainer.style, {
-            'position': 'relative',
-            'display': 'inline-block',
-            'max-width': this.matInputAutosizeMaxWidth || null,
-            'min-width': this.matInputAutosizeMinWidth,
-            'min-height': '57px',
-            'visibility': 'hidden',
-            'white-space': 'pre'
-        });
-        el.parentNode.prepend(relativeContainer);
-
-        this.hiddenText = document.createTextNode(' ');
-        relativeContainer.appendChild(this.hiddenText);
-
-        Object.assign(el.style, {
-            'position': 'absolute',
-            'left': '0',
-            'width': '100%',
-            'visibility': 'visible'
-        });
-        relativeContainer.appendChild(el);
-    }
-
-    ngAfterContentInit() {
-        if (!this.childMatInput) {
-            throw new Error('Element with matInputAutosize directive must also have the matInput directive');
-        }
-
-        if (this.childNgModel) {
-            this.changeDetectSub = this.childNgModel.control.valueChanges.subscribe(this.changeHiddenText);
-        } else {
-            this.element.nativeElement.addEventListener('input', () => this.changeHiddenText(this.element.nativeElement.value));
-        }
+		this.checkErrors();
+		this.overwriteParentWidth();
+		this.createHiddenTextContainer();
+		this.setOwnStyles();
+		this.subscribeToChanges();
     }
 
     ngOnDestroy() {
@@ -77,6 +42,51 @@ export class MatInputAutosizeDirective implements AfterViewInit, AfterContentIni
             this.changeDetectSub.unsubscribe();
         }
     }
+
+    checkErrors() {
+		if (!(this.inputElement instanceof HTMLInputElement)) {
+            throw new Error('matInputAutosize directive must be on an input element');
+        }
+		
+        if (!this.childMatInput) {
+            throw new Error('Element with matInputAutosize directive must also have the matInput directive');
+        }
+    }
+	
+	overwriteParentWidth() {
+		Object.assign((this.inputElement.parentNode as HTMLElement).style, {'width': '100%'});
+	}
+	
+	createHiddenTextContainer() {
+		const textContainer = document.createElement('div');
+        Object.assign(textContainer.style, {
+            'display': 'inline-block',
+            'max-width': this.matInputAutosizeMaxWidth || null,
+            'min-width': this.matInputAutosizeMinWidth,
+			'font': this.matInputAutosizeFontStyle || null,
+            'visibility': 'hidden',
+            'white-space': 'pre'
+        });
+        this.inputElement.parentNode.appendChild(textContainer);
+
+        this.hiddenText = document.createTextNode(' ');
+        textContainer.appendChild(this.hiddenText);
+	}
+	
+	setOwnStyles() {
+		Object.assign(this.inputElement.style, {
+            'position': 'absolute',
+            'left': '0'
+        });
+	}
+	
+	subscribeToChanges() {
+        if (this.childNgModel) {
+            this.changeDetectSub = this.childNgModel.control.valueChanges.subscribe(this.changeHiddenText);
+        } else {
+            this.inputElement.addEventListener('input', () => this.changeHiddenText(this.inputElement.value));
+        }
+	}
 
     changeHiddenText = (newValue: string) => {
         if (this.hiddenText) {
